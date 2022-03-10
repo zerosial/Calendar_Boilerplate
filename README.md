0c2b2944db07400b932f7a8814cb03d0dba064e9
zerosial <zerosial@gmail.com>
2022-03-11 00:53:25 +0900

    3/11

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Calendar_Boilerplate
+
+## _The Last Markdown Editor, zerosial_
+
+Vanilla Coding 스타터 킷 - STEP 5
+
+- HTML , CSS , JS를 이용해 기본적인 웹을 구동한다
+- Readme를 조금 더 쌔련되게 만든다.
+- HTML , CSS 요소는 예제 페이지 참고 (JS는 새로 작성)
+
+## TODO LIST
+
+- 오늘의 현재 요일 표기
+- 오늘의 현재 날짜 표기
+- 오늘의 현재 월 표기
+- 오늘의 현재 연도 표기
+- 일,월,화,수,목,금,토 요일 라벨링 표기
+- 현재 월의 1일이 무슨 요일인지 판별하고, 해당 요일 라벨링에 1일 표기하기
+- 현재 월의 마지막 날짜까지 달력에 표기하기
+- 우측 화살표를 클릭 했을때, 다음 달의 요일 및 날짜 표기
+- 좌측 화살표를 클릭 했을때, 이전 달의 요일 및 날짜 표기
+- 특정 날짜를 클릭 했을때, 상단의 요일 및 날짜 반영하기
+
+## Tech
+
+- [dillinger.io] - 리드미 수정
+- [Visual Studio Code] - 코딩 프로그램
+- [Live-server] - 실시간 서버 반영으로 웹 에디팅의 편리함을 줌
+- [Javascript]
+- [CSS]
+- [HTML]
+
+## License
+
+https://book.vanillacoding.co/starter-kit/step-5/final-projects/calendar
--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("hello, vanilla.");
+const calendarDates = document.querySelectorAll("tbody td");
+const currentDay = document.querySelectorAll(".current-day p");
+const currentMonth = document.querySelector(".current-month");
+const prevButton = document.querySelector(".button.prev");
+const nextButton = document.querySelector(".button.next");
+
+const DAYS = ["일", "월", "화", "수", "목", "금", "토"];
+const MONTHS = [
+  "1월",
+  "2월",
+  "3월",
+  "4월",
+  "5월",
+  "6월",
+  "7월",
+  "8월",
+  "9월",
+  "10월",
+  "11월",
+  "12월",
+];
+
+// 현재 날짜 정보 input
+var today = new Date();
+var dd = today.getDate(); // 날짜 1 ~ 31
+var mm = today.getMonth() + 1; // 달 0 ~ 11
+var day = today.getDay(); // 요일 0 ~ 6
+var yyyy = today.getFullYear(); // 년도 4자리
+
+// 현재 날짜 화면 표시
+currentDay[0].textContent = DAYS[day];
+currentDay[1].textContent = dd;
+currentMonth.textContent = `${mm}월 ${yyyy}`;
