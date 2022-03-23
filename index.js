d2fe11668a40ad5de6eb889cbd935e929c6cc9d8
zerosial <zerosial@gmail.com>
2022-03-23 12:37:37 +0900

    3/23

--- index.js
+++ index.js
@@ -27,7 +27,15 @@ var mm = today.getMonth() + 1; // 달 0 ~ 11
 var day = today.getDay(); // 요일 0 ~ 6
 var yyyy = today.getFullYear(); // 년도 4자리
 
+// 변수정보 입력 화살표 클릭씨 변경되는 변수
+var mmSet = mm;
+var yyyySet = yyyy;
+
 // 현재 날짜 화면 표시
 currentDay[0].textContent = DAYS[day];
 currentDay[1].textContent = dd;
 currentMonth.textContent = `${mm}월 ${yyyy}`;
+
+// 현재 날짜 1일 요일판별 (해당하는 달 1일 날짜 판별)
+var firstDay = new Date(`${mmSet} 1, ${yyyySet} 23:15:30`);
+var firstDate = firstDay.getDay();
